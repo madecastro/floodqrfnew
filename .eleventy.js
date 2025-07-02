63908a03c913b200d1fc861812e5d9aefd43311c
Mark De Castro <mark@zumby.io>
2025-07-01 21:59:26 -0700

    Updating site for images and formatting to include header and footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("static");
+  eleventyConfig.addPassthroughCopy("admin");
   return {
     dir: {
       input: ".",
