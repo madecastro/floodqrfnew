840336e2f5b2223699cda2f02fdddd0be68b0686
Mark De Castro <mark@zumby.io>
2025-07-03 11:48:39 -0700

    another fix to the eleventy template

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,8 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
-      includes: "_includes"
+      includes: "_includes",
+      output: "_site"
     }
   };
 };
