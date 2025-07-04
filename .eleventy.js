1079bb657b00a481e081981f66c679de04c8d62d
Mark De Castro <mark@zumby.io>
2025-07-04 00:26:36 -0700

    another attempt

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/admin");
@@ -5,8 +6,8 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",
-      includes: "_includes",
-      output: "_site"
+      output: "_site",
+      includes: "_includes"
     }
   };
 };
