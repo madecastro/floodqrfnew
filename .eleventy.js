067b2da0f7a6daf1874e0d074f9d2ef959729e37
Mark De Castro <mark@zumby.io>
2025-07-03 21:24:03 -0700

    redo of styling

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,13 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/admin");
 
+  eleventyConfig.addTransform("preserveScripts", function (content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
+      return content.replace("<script ", "<script ");
+    }
+    return content;
+  });
+
   return {
     dir: {
       input: "src",
